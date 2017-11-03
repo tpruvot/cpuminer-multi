af1c1d233de2fb3c454cf91459471eaff01b18a9
Daniel Andersen <public@andersen.pub>
2017-11-03 12:30:05 +0000

    aligned cornz implementation of keccakc into cpu-miner multi by dan-and

--- README.md
+++ README.md
@@ -69,6 +69,7 @@ Algorithms
 #### Implemented, but untested
  * ? hefty1 (Heavycoin)
  * ? keccak (Maxcoin  HelixCoin, CryptoMeth, Galleon, 365coin, Slothcoin, BitcointalkCoin)
+ * ? keccakc (Creativecoin)
  * ? luffa (Joincoin, Doomcoin)
  * ? shavite3 (INKcoin)
 
@@ -186,6 +187,7 @@ Credits
 CPUMiner-multi was forked from pooler's CPUMiner, and has been started by Lucas Jones.
 * [tpruvot](https://github.com/tpruvot) added all the recent features and newer algorythmns
 * [Wolf9466](https://github.com/wolf9466) helped with Intel AES-NI support for CryptoNight
+* [cornz](https://github.com/cornz) showed how to add keccakc based on keccak. A
 
 License
 =======
--- cpu-miner.c
+++ cpu-miner.c
@@ -78,6 +78,7 @@ struct workio_cmd {
 
 enum algos {
 	ALGO_KECCAK,      /* Keccak */
+	ALGO_KECCAKC,     /* KeccakC */
 	ALGO_HEAVY,       /* Heavy */
 	ALGO_NEOSCRYPT,   /* NeoScrypt(128, 2, 1) with Salsa20/20 and ChaCha20/20 */
 	ALGO_QUARK,       /* Quark */
@@ -133,6 +134,7 @@ enum algos {
 
 static const char *algo_names[] = {
 	"keccak",
+	"keccakc",
 	"heavy",
 	"neoscrypt",
 	"quark",
@@ -305,6 +307,7 @@ Options:\n\
                           heavy        Heavy\n\
                           jha          JHA\n\
                           keccak       Keccak\n\
+                          keccakc      KeccakC (Creativecoin)\n\
                           luffa        Luffa\n\
                           lyra2re      Lyra2RE\n\
                           lyra2rev2    Lyra2REv2 (Vertcoin)\n\
@@ -1803,6 +1806,9 @@ static void stratum_gen_work(struct stratum_ctx *sctx, struct work *work)
 				work_set_target(work, sctx->job.diff / (256.0 * opt_diff_factor));
 				break;
 			case ALGO_KECCAK:
+			case ALGO_KECCAKC:
+				work_set_target(work, sctx->job.diff / (256.0 * opt_diff_factor));
+				break:
 			case ALGO_LYRA2:
 				work_set_target(work, sctx->job.diff / (128.0 * opt_diff_factor));
 				break;
@@ -2225,6 +2231,9 @@ static void *miner_thread(void *userdata)
 		case ALGO_KECCAK:
 			rc = scanhash_keccak(thr_id, &work, max_nonce, &hashes_done);
 			break;
+		case ALGO_KECCAKC:
+			rc = scanhash_keccak(thr_id, &work, max_nonce, &hashes_done);
+			break:
 		case ALGO_HEAVY:
 			rc = scanhash_heavy(thr_id, &work, max_nonce, &hashes_done);
 			break;
