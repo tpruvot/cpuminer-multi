f3a088b8fba1dfee6e05b7126222f11af9a848a0
sapphire-pt <sapphire_pt@sappcoin.com>
2020-08-21 03:17:36 +0100

    X11K adjustments

--- algo/x11k.c
+++ algo/x11k.c
@@ -32,122 +32,122 @@ const uint64_t GetUint64(const void *data, int pos)
 			((uint64_t)ptr[7]) << 56;
 }
 
-void *Blake512(void *oHash, const void *iHash)
+void *Blake512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_blake512_context ctx_blake;
 
 	sph_blake512_init(&ctx_blake);
-	sph_blake512 (&ctx_blake, iHash, 80);
+	sph_blake512 (&ctx_blake, iHash, len);
 	sph_blake512_close (&ctx_blake, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Bmw512(void *oHash, const void *iHash)
+void *Bmw512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_bmw512_context ctx_bmw;
 
 	sph_bmw512_init(&ctx_bmw);
-	sph_bmw512 (&ctx_bmw, iHash, 64);
+	sph_bmw512 (&ctx_bmw, iHash, len);
 	sph_bmw512_close(&ctx_bmw, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Groestl512(void *oHash, const void *iHash)
+void *Groestl512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_groestl512_context ctx_groestl;
 
 	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512 (&ctx_groestl, iHash, 64);
+	sph_groestl512 (&ctx_groestl, iHash, len);
 	sph_groestl512_close(&ctx_groestl, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Skein512(void *oHash, const void *iHash)
+void *Skein512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_skein512_context ctx_skein;
 
 	sph_skein512_init(&ctx_skein);
-	sph_skein512 (&ctx_skein, iHash, 64);
+	sph_skein512 (&ctx_skein, iHash, len);
 	sph_skein512_close (&ctx_skein, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Jh512(void *oHash, const void *iHash)
+void *Jh512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_jh512_context ctx_jh;
 
 	sph_jh512_init(&ctx_jh);
-	sph_jh512 (&ctx_jh, iHash, 64);
+	sph_jh512 (&ctx_jh, iHash, len);
 	sph_jh512_close(&ctx_jh, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Keccak512(void *oHash, const void *iHash)
+void *Keccak512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_keccak512_context ctx_keccak;
 
 	sph_keccak512_init(&ctx_keccak);
-	sph_keccak512 (&ctx_keccak, iHash, 64);
+	sph_keccak512 (&ctx_keccak, iHash, len);
 	sph_keccak512_close(&ctx_keccak, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Luffa512(void *oHash, const void *iHash)
+void *Luffa512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_luffa512_context ctx_luffa1;
 
 	sph_luffa512_init (&ctx_luffa1);
-	sph_luffa512 (&ctx_luffa1, iHash, 64);
+	sph_luffa512 (&ctx_luffa1, iHash, len);
 	sph_luffa512_close (&ctx_luffa1, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Cubehash512(void *oHash, const void *iHash)
+void *Cubehash512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_cubehash512_context	ctx_cubehash1;
 
 	sph_cubehash512_init (&ctx_cubehash1);
-	sph_cubehash512 (&ctx_cubehash1, iHash, 64);
+	sph_cubehash512 (&ctx_cubehash1, iHash, len);
 	sph_cubehash512_close(&ctx_cubehash1, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Shavite512(void *oHash, const void *iHash)
+void *Shavite512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_shavite512_context ctx_shavite1;
 
 	sph_shavite512_init (&ctx_shavite1);
-	sph_shavite512 (&ctx_shavite1, iHash, 64);
+	sph_shavite512 (&ctx_shavite1, iHash, len);
 	sph_shavite512_close(&ctx_shavite1, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Simd512(void *oHash, const void *iHash)
+void *Simd512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_simd512_context	ctx_simd1;
 
 	sph_simd512_init (&ctx_simd1);
-	sph_simd512 (&ctx_simd1, iHash, 64);
+	sph_simd512 (&ctx_simd1, iHash, len);
 	sph_simd512_close(&ctx_simd1, pHash);
 
 	memcpy(oHash, pHash, 64);
 }
 
-void *Echo512(void *oHash, const void *iHash)
+void *Echo512(void *oHash, const void *iHash, const size_t len)
 {
 	sph_echo512_context	ctx_echo1;
 
 	sph_echo512_init (&ctx_echo1);
-	sph_echo512 (&ctx_echo1, iHash, 64);
+	sph_echo512 (&ctx_echo1, iHash, len);
 	sph_echo512_close(&ctx_echo1, pHash);
 
 	memcpy(oHash, pHash, 64);
@@ -167,12 +167,12 @@ void *fnHashX11K[] = {
 	Echo512,
 };
 
-void processHash(void *oHash, const void *iHash, int index)
+void processHash(void *oHash, const void *iHash, const int index, const size_t len)
 {
-	void (*hashX11k)(void *oHash, const void *iHash);
+	void (*hashX11k)(void *oHash, const void *iHash, const size_t len);
 
 	hashX11k = fnHashX11K[index];
-	(*hashX11k)(oHash, iHash);
+	(*hashX11k)(oHash, iHash, len);
 }
 
 void x11khash(void *output, const void *input)
@@ -182,11 +182,11 @@ void x11khash(void *output, const void *input)
 	uint32_t _ALIGN(64) hashA[16], hashB[16];
 
 	// Iteration 0
-	processHash(hashA, input, 0);
+	processHash(hashA, input, 0, 80);
 
 	for(int i = 1; i < HASHX11K_NUMBER_ITERATIONS; i++) {
 		uint64_t index = GetUint64(hashA, i % 8) % 11;
-		processHash(hashB, hashA, index);
+		processHash(hashB, hashA, index, 64);
 		memcpy(hashA, hashB, 64);
 	}
 
